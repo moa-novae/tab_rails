4cb4e6bb98897fe8b34a5589c7158e7220243744
Pumpkin-spice-cloud <alan_mo@live.com>
2020-07-06 16:55:46 -0400

    style fix

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < Devise::SessionsController
   respond_to :json
   private
   def respond_with(resource, _opts = {})
-      render json: resource
+    render json: resource
     enddef respond_to_on_destroy
-      head :ok
-    end
+    head :ok
+  end
 end
